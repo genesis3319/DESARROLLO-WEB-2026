177fc03cad17288b80655ceeacafc90a7f83801a
Genesis Nikolle Andy Chimbo <gn.andyc@uea.edu.ec>
2026-09-25 17:55:20 -0500

    Configuracion para despliegue en Render

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -8,14 +8,24 @@ import psycopg2
 
 def obtener_conexion():
 
-    # Crear la conexión con la base de datos PostgreSQL
-    conexion = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        user="postgres",
-        password=os.environ.get("POSTGRES_PASSWORD"),
-        database="arte_mostacilla"
-    )
+    # Obtener la URL de PostgreSQL de Render.
+    # En la computadora local esta variable normalmente no existe.
+    database_url = os.environ.get("DATABASE_URL")
+
+    # Si existe DATABASE_URL, significa que estamos usando
+    # la base de datos configurada para Render.
+    if database_url:
+        conexion = psycopg2.connect(database_url)
+
+    # Si no existe DATABASE_URL, utilizar PostgreSQL local.
+    else:
+        conexion = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            user="postgres",
+            password=os.environ.get("POSTGRES_PASSWORD"),
+            database="arte_mostacilla"
+        )
 
     # Retornar la conexión para utilizarla en app.py
     return conexion
